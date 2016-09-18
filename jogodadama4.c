bc73bea102c3815a02e6a3fa7bc3ff83c57281bb
Renan <renan4573@hotmail.com>
2016-09-17 23:57:28 -0300

    modificações

--- jogodadama4.c
+++ jogodadama4.c
@@ -2,24 +2,20 @@
 #include <stdlib.h>
 #define N 8
 /* preenche o tabuleiro com peças e printa */
-void printmatriz (char **M) {
+void printMatriz(char **M){
     int i,j;
+    printf("\n  0  1  2  3  4  5  6  7\n");
 
-     printf("   0  1  2  3  4  5  6  7 \n");
-
-     for (i = 0; i < N; i++) {
-      printf("%d ", i);
-        for (j = 0; j < N; j++) {
-          if (i < 3 && (i + j) %2 != 0) {
-            M[i][j] ='B';
-          }
-          if (i > 4 && (i+j) %2 !=0)
-            M[i][j] ='P';
-           printf("[%c]",M[i][j]);
-          }
+    for (i = 0; i < N; i++)
+    {
+      printf("%d",i);
+        for(j = 0; j < N; j++)
+        {
+            printf("[%c]",M[i][j]);
+        }
         printf("\n");
-     }
-}
+    }
+}     
 char **initialize () {
   /* alocação das linhas e colunas */
   char **M;
@@ -29,55 +25,171 @@ char **initialize () {
         M[i] = (char *)calloc(N, sizeof(char));
 
     for(i = 0; i < N; i++)
+      {
         for(j = 0; j < N; j++)
-            M[i][j]=' ';//espaco em branco
+        {
+          if (i == 0 || i == 2)
+          {
+            if(j%2 == 0)
+            {
+              M[i][j]=' ';
+            }
+            else 
+            {
+              M[i][j]='B';
+            }
+          }
+          else if (i == 1)
+          {
+            if (j%2 == 0)
+            {
+              M[i][j]='B';
+            }
+             else 
+            {
+              M[i][j]=' ';
+            }
+          }
+          if (i == 5 || i == 7)
+          {
+            if(j%2 == 0)
+            {
+              M[i][j]='P';
+            }
+            else 
+            {
+              M[i][j]=' ';
+            }
+          }
+          else if (i == 6)
+          {
+            if (j%2 == 0)
+            {
+              M[i][j]=' ';
+            }
+             else 
+            {
+              M[i][j]='P';
+            }
+          }
+          if (i == 4 || i == 3)
+          {
+            M[i][j]=' ';
+          }
+       }
+
+    }        
     return M;
 }
+/*condições para a realização de jogadas e condições para poder comer */
 int step (char **M, int linha,int coluna,int linhadest,int coldest, char jogador) {
-  if(jogador == 'P') 
-  {
-    if ((linha+coluna)%2!=0) 
-    { 
-     if((linhadest < linha)||
-   
-      return 1;
-  }
-}
-void game () {
+  int i,j;
+        //para mover não deve retornar 0
+        if (linha < 0 || linhadest < 0 || 
+            coluna < 0 || coldest < 0 || 
+            linha >= N || linhadest >= N ||
+            coluna >= N || coldest >= N) 
+          { 
+            return 0;
+          } 
+        if ((linha+coluna)%2 == 0 || (linhadest+coldest)%2 == 0) 
+          {
+         return 0; 
+          }
+
+        if (jogador == 'B') 
+          {
+          if (
+              linha > linhadest ||
+              M[linha][coluna] == 'P' || 
+              M[linhadest][coldest] !=' ')
+          {
+            return 0;
+          } 
+           else if(linhadest > linha+1)//condições de B para poder comer 
+          { 
+            if (M[linha+2][coluna+2] ==' ' && M[linha+1][coluna+1] =='P')
+            {
+              M[linha][coluna] =' ';
+              M[linha+1][coluna+1] =' ';
+              M[linhadest][coldest] ='B';
+              return 1;   
+            } 
+            if (M[linha+2][coluna-2] ==' ' && M[linha+1][coluna-1] =='P')
+             {
+              M[linha][coluna] =' ';
+              M[linha+1][coluna-1] =' ';
+              M[linhadest][coldest] ='B';
+              return 1;
+             }
+          } 
+         } 
+        if (jogador == 'P')
+        {
+          if( 
+             linha < linhadest ||
+             M[linha][coluna] == 'B' || 
+             M[linhadest][coldest] !=' ')
+          {
+            return 0;
+        } 
+         else if(linhadest == linha-1)//condições de P para poder comer 
+          { 
+            if (M[linha-2][coluna+2] ==' ' && M[linha-1][coluna+1] =='B')
+            {
+              M[linha][coluna] =' ';
+              M[linha-1][coluna+1] =' ';
+              M[linhadest][coldest] ='P';
+              return 1;   
+            } 
+            if (M[linha-2][coluna-2] ==' ' && M[linha-1][coluna-1] =='B')
+             {
+              M[linha][coluna] =' ';
+              M[linha-1][coluna-1] =' ';
+              M[linhadest][coldest] ='P';
+              return 1;
+             }
+          }
+         }
+
+     M[linha][coluna] = ' '; 
+     M[linhadest][coldest] = jogador;   
+        return 1;
+       }
+void game () {  
   char **tabuleiro;
   int linha, coluna,lind,cold;
-  char jogador = 'P';
+  char jogador = 'B';
   int jogoativo = -1;
   
   tabuleiro = initialize();
-  /* laço jogo da dama */
-     
-    printmatriz(tabuleiro);
+
     printf("\nBem vindo ao jogo!!\n");
-    while (jogoativo==-1) {
-      if (jogador == 'P')
-        printf("Vamos jogador P,faça seus movimentos\n");
-      if (jogador == 'B') 
-        printf("Vamos jogador B,faça seus movimentos\n");
-    scanf("%d %d", &linha, &coluna);
-    printf("Destino: ");
-    scanf("%d %d", &lind, &cold);
-    step(tabuleiro,linha, coluna, lind, cold, jogador);
-    // se não posso prosseguir
-    if ( !step(tabuleiro,linha, coluna, lind, cold, jogador)) {
-      printf("Jogada Invalida!\n");
+    while (jogoativo==-1) 
+    {
+      printMatriz(tabuleiro);
+      if (jogador == 'B')
+      printf("\nVamos jogador B,faça seus movimentos\n");
+      if (jogador == 'P') 
+      printf("\nVamos jogador P,faça seus movimentos\n");
+      printf("Origem: ");
+      scanf("%d %d", &linha, &coluna);
+      printf("Destino: ");
+      scanf("%d %d", &lind, &cold);
+      // caso a jogada seja errada
+      if (!step(tabuleiro,linha, coluna, lind, cold, jogador)) {     
+        printf("\nJogada Invalida!jogue novamente\n");
       continue;
     }
-    //alternancia de jogador
-    if (jogador == 'P')
-        jogador = 'B';
-      else jogador = 'P';
-      printmatriz(tabuleiro);
-    }
-    
-}  
 
-void main () {
+
+    //alternancia de jogador
+    if (jogador == 'B')
+        jogador = 'P';
+      else jogador = 'B';
+    }
+   }   
+int main (void) {
 
   game();
 }  
